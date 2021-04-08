061a19fa16d61fce49e10950df3993a532952a29
YumiMachino <y.y.1515m@gmail.com>
2021-04-08 12:43:39 -0700

    add DAG topological sort assignment2

--- Algorithm&DataStructures/Assignment/DAG TopologicalSort/CourseScheduleII.swift
+++ Algorithm&DataStructures/Assignment/DAG TopologicalSort/CourseScheduleII.swift
@@ -1,19 +1,20 @@
 //
-//  CourseSchedule.swift
+//  CourseScheduleII.swift
 //  Algorithm&DataStructures
 //
-//  Created by Yumi Machino on 2021/04/07.
+//  Created by Yumi Machino on 2021/04/08.
 //
 
 import Foundation
 
-func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+    
     //store undirected graph
-    var adj = [[Int]](repeating: [Int](), count: numCourses - 1)
+    var adj = [[Int]](repeating: [Int](), count: numCourses)
     // create a list of indeg
-   
-    var indegree = [Int](repeating: 0, count: numCourses - 1)
-
+    var indegree = [Int](repeating: 0, count: numCourses)
+    var output = [Int]()
+    
     for i in 0..<numCourses  {
         // create adj list
         let v = prerequisites[i][0]
@@ -21,30 +22,25 @@ func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
         // increment indegree count
         indegree[v] += 1
     }
-        
+    print("adjList", adj)
+    print("indegree", indegree)
     // Topological Sort - using BFS(Queue)
     let q = Queue<Int>()
     for  v in 0..<indegree.count {
-        if indegree[v] == 0 {
-            q.enqueue(item: v)
+        if indegree[v] == 0 {            q.enqueue(item: v)
         }
     }
     
+    // BFS
     while !q.isEmpty() {
         let u = q.dequeue()!
-        for v in prerequisites[u] {
+        output.append(u)
+        for v in adj[u] {
             indegree[v] -= 1
             if indegree[v] == 0 {
                 q.enqueue(item: v)
             }
         }
     }
-    
-    for i in 0...indegree.count {
-        if indegree[i] != 0 {
-            return false
-        }
-    }
-    return true
+    return output
 }
-
--- Algorithm&DataStructures/main.swift
+++ Algorithm&DataStructures/main.swift
@@ -84,4 +84,5 @@ import Foundation
 //temp()
 //sushiReview()
 //tomatoFarm()
-print(canFinish(4, [[1,0],[2,1], [2,3], [3,2]]))
+//print(canFinish(4, [[1,0],[2,1],[3,2],[2,3]]))
+print(findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
