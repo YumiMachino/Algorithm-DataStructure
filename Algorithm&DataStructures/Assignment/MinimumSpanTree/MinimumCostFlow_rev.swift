d7e26d91f5dc99c02e0bf17a1bbc6133931655a0
YumiMachino <y.y.1515m@gmail.com>
2021-04-16 12:08:46 -0700

    refactor

--- Algorithm&DataStructures/Assignment/MinimumSpanTree/MinimumCostFlow_rev.swift
+++ Algorithm&DataStructures/Assignment/MinimumSpanTree/MinimumCostFlow_rev.swift
@@ -44,9 +44,8 @@ extension Edge: Comparable {
 
 extension Edge: Hashable {}
 
-
 func inputHander() -> (Int,Int, [Edge]){
-    // create Edge list bt input
+    // create Edge list by input
     let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
     let n = firstLine[0]
     let m = firstLine[1]
@@ -74,12 +73,12 @@ func kruskalMSTHelper(_ sortedEdgeList: [Edge], _ n: Int, _ d: Int) -> (Int, [Ed
     var unionFind = UnionFind(sortedEdgeList.count)
     
     for i in 0..<sortedEdgeList.count - 1 {
-        var edge = sortedEdgeList[i]
+        let edge = sortedEdgeList[i]
         if unionFind.connected(edge.u, edge.v) { continue }
         unionFind.union(edge.u, edge.v)
-        //last node: highest cost
+        ///last node: highest cost
         if pickedEdges.count == n - 1 {
-            // 残りのやつ全部にuse enhancer, sort-> select minimum as last node
+            // use enhancer for the rest, sort-> select minimum as last node
             for j in 0..<remain.count {
                 if remain[j].w > d {
                     remain[j].w = remain[j].w - d
@@ -167,68 +166,5 @@ public struct UnionFind {
 }
 
 
-
-
-
-func testHandler() {
-    let fileManager = FileManager()
-    let testFolder = "/Users/yumi/Documents/CICCC/Algorithm&DataStructures/Algorithm&DataStructures/Assignment/MinimumSpanTree/mcfTest"
-    
-    var i = 0
-    if let filePath = try? fileManager.contentsOfDirectory(atPath: testFolder).sorted() {
-        while i < filePath.count {
-            let url = URL(fileURLWithPath: testFolder + "/" + filePath[i])
-            let input = try? String(contentsOf: url, encoding: .utf8)
-            let testInput = input!.split(separator: "\n").map { String($0) }
-            print(testInput)
-            var firstLine = testInput[0]
-            var intFirstLine = [Int]()
-            
-            for j in 0..<firstLine.count {
-                if firstLine[j..<j + 1] == " " {
-                    continue
-                } else {
-                    intFirstLine.append(Int(firstLine[j..<j + 1])!)
-                }
-            }
-            print("intFirstLine: ", intFirstLine)
-            let n = intFirstLine[0]
-            let m = intFirstLine[1]
-            let d = intFirstLine[2]
-            var edgeList = [Edge]()
-            
-            for i in 1..<testInput.count - 1 {
-                let edge = testInput[i]
-                testInput[i].filter { Int($0)? Int($0) : ""}
-                
-                let u = Int(edge[0])!
-                let v = Int(edge[1])!
-                let w = Int(edge[2])!
-
-                if i >= n - 1 {
-                    edgeList.append(Edge(u: u, v: v, w: w, active: 0))
-                }
-                edgeList.append(Edge(u: u, v: v, w: w, active: 1))
-            }
-            return (n,d,edgeList)
-            
-        }
-    }
-}
-
-
-extension String {
-    subscript(_ range: CountableRange<Int>) -> String {
-        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
-        let end = index(start, offsetBy: min(self.count - range.lowerBound,
-                                             range.upperBound - range.lowerBound))
-        return String(self[start..<end])
-    }
-
-    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
-        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
-         return String(self[start...])
-    }
-}
 
 
